4385750dd184302d6b04e08ad0a942ae8ef8e1c0
SamirChen <sheng4893166@126.com>
2016-10-10 16:19:39 +0800

    Add ffmpeg branch.

--- init-ffmpeg.sh
+++ init-ffmpeg.sh
@@ -1,11 +1,12 @@
 #! /usr/bin/env bash
 
 IJK_FFMPEG_ORIGIN=https://github.com/samirchen/FFmpeg.git
+IJK_FFMPEG_BRANCH=ijk-r0.4.5-ff3.0-dev
 LOCAL_WORKSPACE=ffmpeg
 
 
 if [ ! -d $LOCAL_WORKSPACE ]; then
-    git clone $IJK_FFMPEG_ORIGIN $LOCAL_WORKSPACE
+    git clone -b $IJK_FFMPEG_BRANCH $IJK_FFMPEG_ORIGIN $LOCAL_WORKSPACE
 else
     cd $LOCAL_WORKSPACE
     git fetch --all --tags
